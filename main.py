d025a5099b05ab3a0d876c9dee149c398db2ea9b
Junha <hjshljy@gmail.com>
2023-11-24 09:45:56 +0900

    Init Commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+
+from enum import Enum
+from typing import Union
+
+
+class ModelNames(str, Enum):
+    ALEXNET = "alexnet"
+    RESNET = "resnet"
+    LENET = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name:  ModelNames):
+    if model_name is ModelNames.ALEXNET:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have som e residuals"}
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({
+            "description": "This is an amazing item that has a long description"
+        })
+    return item
+
